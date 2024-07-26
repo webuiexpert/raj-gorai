6ae01a0cf167bf3e598918b40860a3315a7e4bd0
webuiexpert <webuiexpert929@gmail.com>
2024-07-26 18:37:15 +0530

    some changes

--- src/components/SolveBtnSection.jsx
+++ src/components/SolveBtnSection.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function SolveBtnSection() {
   return (
-    <div className="solveBtn w-full bg-[#174153] lg:py-20 py-10 flex flex-col items-center">
-      <h2 className="lg:text-4xl text-3xl text-center font-bold text-white">
+    <div className="solveBtn w-full bg-[#174153] lg:py-20 py-10 flex flex-col items-center md:px-8 sm:px-6 px-4">
+      <h2 className="lg:text-4xl text-[24px] text-center font-bold text-white">
         Worried about how to grow your business Online?
       </h2>
-      <h2 className="lg:text-4xl text-3xl text-center font-bold text-white">
+      <h2 className="lg:text-4xl text-[24px] text-center font-bold text-white">
         Let's Solve your problem.
       </h2>
-      <button className="mt-10 text-[18px] leading-8 px-16 py-2 border text-white font-bold bg-[#13a300]">
+      <button className="lg:mt-10 mt-5 lg:text-[18px] text-[16px] leading-8 lg:px-16 px-4 py-2 border text-white font-bold bg-[#13a300]">
         Book an Appointment Today!!
       </button>
     </div>
