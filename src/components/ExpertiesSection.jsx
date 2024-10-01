1b5e62b6bd26241c1a66d4f448e07e7b1d48e201
webuiexpert <webuiexpert929@gmail.com>
2024-10-01 11:23:34 +0530

    add about and expertise section content

--- src/components/ExpertiesSection.jsx
+++ src/components/ExpertiesSection.jsx
@@ -10,19 +10,19 @@ function ExpertiesSection() {
               <div className="box">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Global SEO</h3>
-                    <li>Specialized in multilingual SEO projects. Scaling Business expands their visibility in different countries with the right recommendations to dominate SERPs.</li>
+                    <li>I help brands reach international audiences by optimizing websites for global search engines, ensuring effective keyword targeting and content localization.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Local SEO</h3>
-                    <li>Optimize your website & GMB with local ranking factors to BOOST your organic visibility to Rank High on SERPs and generate more local business!</li>
+                    <li>I specialize in improving search visibility for local businesses by optimizing for location-based queries and ensuring businesses rank high in local search results.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Ecommerce SEO</h3>
-                    <li>Ecommerce SEO is the process of enhancing the visibility of your online store when people are searching for your products & categories on SERP.</li>
+                    <li>I enhance the visibility of ecommerce platforms by optimizing product pages, categories, and user experience, driving organic traffic and increasing conversions.</li>
                 </ul>
               </div>
             </div>
@@ -30,19 +30,19 @@ function ExpertiesSection() {
               <div className="box lg:mt-0">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Content Strategy</h3>
-                    <li>Specialized in multilingual SEO projects. Scaling Business expands their visibility in different countries with the right recommendations to dominate SERPs.</li>
+                    <li>I develop content plans that align with SEO goals, focusing on creating engaging, keyword-optimized content that drives organic traffic and enhances user engagement.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Technical SEO</h3>
-                    <li>Optimize your website & GMB with local ranking factors to BOOST your organic visibility to Rank High on SERPs and generate more local business!</li>
+                    <li>I ensure websites are search engine-friendly by addressing technical aspects like site speed, mobile optimization, structured data, and indexing.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Link Building & PR</h3>
-                    <li>Ecommerce SEO is the process of enhancing the visibility of your online store when people are searching for your products & categories on SERP.</li>
+                    <li>I build high-quality backlinks through strategic partnerships and digital PR campaigns, boosting domain authority and improving search rankings.</li>
                 </ul>
               </div>
             </div>
@@ -50,19 +50,19 @@ function ExpertiesSection() {
               <div className="box lg:mt-0">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">GMB Listing</h3>
-                    <li>Specialized in multilingual SEO projects. Scaling Business expands their visibility in different countries with the right recommendations to dominate SERPs.</li>
+                    <li>I optimize Google My Business profiles to ensure businesses rank well in local searches and appear in Google Maps, increasing visibility for nearby customers.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Citation building</h3>
-                    <li>Optimize your website & GMB with local ranking factors to BOOST your organic visibility to Rank High on SERPs and generate more local business!</li>
+                    <li>I enhance local search presence by ensuring consistent and accurate listings across various online directories, helping to improve local SEO performance.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Amazon SEO</h3>
-                    <li>Ecommerce SEO is the process of enhancing the visibility of your online store when people are searching for your products & categories on SERP.</li>
+                    <li>I optimize product listings on Amazon, improving keyword placement, product descriptions, and reviews to boost rankings and increase sales on the platform.</li>
                 </ul>
               </div>
             </div>
@@ -72,19 +72,19 @@ function ExpertiesSection() {
               <div className="box lg:mt-0">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Google Ads</h3>
-                    <li>Specialized in multilingual SEO projects. Scaling Business expands their visibility in different countries with the right recommendations to dominate SERPs.</li>
+                    <li>I create and manage high-performing Google Ads campaigns, optimizing keywords, ad copy, and targeting to drive quality traffic and maximize ROI across search and display networks.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Facebook Ads</h3>
-                    <li>Optimize your website & GMB with local ranking factors to BOOST your organic visibility to Rank High on SERPs and generate more local business!</li>
+                    <li>I design targeted Facebook Ads campaigns that resonate with specific audiences, leveraging precise demographic and interest-based targeting to boost brand awareness, engagement, and conversions.</li>
                 </ul>
               </div>
               <div className="box lg:mt-0 mt-5">
                 <ul>
                     <h3 className="font-bold text-[24px] leading-[1em]">Amazon PPC</h3>
-                    <li>Ecommerce SEO is the process of enhancing the visibility of your online store when people are searching for your products & categories on SERP.</li>
+                    <li>I optimize Amazon PPC campaigns to increase product visibility, drive sales, and improve ad spend efficiency through strategic keyword bidding and performance analysis.</li>
                 </ul>
               </div>
             </div>
