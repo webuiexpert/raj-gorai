f8b9cf0fcaa9478fccc52f02093219470360249c
webuiexpert <webuiexpert929@gmail.com>
2025-03-12 10:21:24 +0530

    some changes on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,11 +10,11 @@ function Footer() {
   return (
     <div id="book-appointment" className="w-full h-auto">
       <div className="max-w-[1320px] mx-auto lg:py-3 py-10 md:px-8 sm:px-6 px-4 flex items-center flex-col gap-3">
-      <p>© 2016-24 Raj-gorai.com All Rights Reserved.</p>
+      <p>© 2016-25 Raj-gorai.com All Rights Reserved.</p>
 
         <div className="flex gap-5">
         <a className="hover:-mt-1 duration-500 " href="https://www.facebook.com/seondigitalmarketingexpert"><FaFacebookF /></a> 
-        <a className="hover:-mt-1 duration-500 " href="#"><FaLinkedinIn /></a> 
+        <a className="hover:-mt-1 duration-500 " href="https://www.linkedin.com/in/seo-digital-marketing-expert-consultant/"><FaLinkedinIn /></a> 
         </div>
       </div>
       <div className="whatsaapDiv fixed right-16 bottom-3">
